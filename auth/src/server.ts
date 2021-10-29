abd007f6725a1a3860b22f37a0c200c2e44a497e
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-29 18:47:27 +0300

    :art: chore: kill server on unhandled events

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import logger from './lib/logger';
 import App from './app';
 import dotenv from 'dotenv';
 
@@ -12,3 +13,26 @@ mongoose();
 
 const server = http.createServer(App);
 server.listen(port);
+
+const gracefulStopServer = function () {
+  // Wait 10 secs for existing connection(s) to close and then exit.
+  setTimeout(() => {
+    logger.info('Shutting down server');
+    process.exit(0);
+  }, 1000);
+};
+
+// Kill server on unhandled exceptions and rejections.
+
+process.on('uncaughtException', (err) => {
+  logger.error(`Uncaught exception: ${err}`);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  logger.error(`unhandledRejection: ${err}`);
+  process.exit(1);
+});
+
+process.on('SIGINT', gracefulStopServer);
+process.on('SIGTERM', gracefulStopServer);
