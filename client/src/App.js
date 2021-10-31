4e028289829bda1fc0a2cf97463823512e579f16
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-31 21:19:12 +0300

    :lipstick: styles: create more questions page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import AboutYouPage from "./pages/AboutYouPage/AboutYouPage";
+import MoreQuestionsPage from "./pages/OnboardingPages/MoreQuestionsPage/MoreQuestionsPage";
 
 function App() {
   return (
     <Router>
       <Route path="/register" component={RegisterPage}></Route>
       <Route path="/onboarding/1" component={AboutYouPage}></Route>
+      <Route path="/onboarding/2" component={MoreQuestionsPage}></Route>
     </Router>
   );
 }
---
+++ client/src/pages/OnboardingPages/MoreQuestionsPage/MoreQuestionsPage.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import styles from "./MoreQuestionsPage.module.css";
+
+export default function MoreQuestionsPage(props) {
+  const onCompleteHandler = () => {
+    props.history.push("/onboarding/3");
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Link to="/onboarding/1" className="link link-back">
+        <i class="fas fa-chevron-left fa-fw fa-xs"></i> Back
+      </Link>
+      <h1 className={styles.page_title}>Hi User!</h1>
+      <main>
+        <p className={`align-center ${styles.info}`}>
+          Just a few more questions so we can suggest experiences you will love
+          and match you with like-minded guys who have similar interests as you.
+        </p>
+      </main>
+      <button
+        className={`btn btn-primary ${styles.action_button}`}
+        onClick={onCompleteHandler}
+      >
+        Continue
+      </button>
+    </div>
+  );
+}
