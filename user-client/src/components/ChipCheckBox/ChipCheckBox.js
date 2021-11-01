8dff8abb91da3455dfd321ceff1c8875e2a75057
Ajaydip Singh <singh@minerva.kgi.edu>
2021-11-01 22:32:48 +0300

    :art: chore: add state to interests

--- user-client/src/components/ChipCheckBox/ChipCheckBox.js
+++ user-client/src/components/ChipCheckBox/ChipCheckBox.js
@@ -1,9 +1,19 @@
 import styles from "./ChipCheckBox.module.css";
 
-export default function ChipCheckBox({ label, imageSrc }) {
+export default function ChipCheckBox({
+  checkboxState,
+  setCheckboxState,
+  label,
+  imageSrc,
+}) {
   return (
     <div className={styles.chip}>
-      <input type="checkbox" id={label} />
+      <input
+        defaultChecked={checkboxState}
+        onChange={(e) => setCheckboxState(!checkboxState)}
+        type="checkbox"
+        id={label}
+      />
       <label for={label}>
         <img className={styles.image} src={imageSrc} alt={label} />
         {label}
--- user-client/src/pages/OnboardingPages/InterestsPage/InterestsPage.js
+++ user-client/src/pages/OnboardingPages/InterestsPage/InterestsPage.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import ChipCheckBox from "../../../components/ChipCheckBox/ChipCheckBox";
 import styles from "./InterestsPage.module.css";
 
 export default function InterestsPage(props) {
+  const [natureAndOutdoors, setNatureAndOutdoors] = useState(false);
+  const [resortVacations, setResortVacations] = useState(false);
+  const [wildlife, setWildlife] = useState(false);
+  const [luxuryGetAway, setLuxuryGetAway] = useState(false);
+  const [camping, setCamping] = useState(false);
+  const [burningMan, setBurningMan] = useState(false);
+  const [musicFestivals, setMusicFestivals] = useState(false);
+  const [artAndCulture, setArtAndCulture] = useState(false);
+  const [prideEvents, setPrideEvents] = useState(false);
+  const [wellnessRetreats, setWellnessRetreats] = useState(false);
+  const [volunteeringTrips, setVolunteeringTrips] = useState(false);
+  const [cruises, setCruises] = useState(false);
+  const [nudistAdventures, setNudistAdventures] = useState(false);
+
   const onCompleteHandler = () => {
-    props.history.push("/onboarding/3");
+    props.history.push("/onboarding/4");
   };
 
   return (
@@ -20,58 +35,86 @@ export default function InterestsPage(props) {
         </p>
         <div className={styles.interests_container}>
           <ChipCheckBox
+            checkboxState={natureAndOutdoors}
+            setCheckboxState={setNatureAndOutdoors}
             label="Nature & Outdoors"
             imageSrc="/assets/icons/rocks.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={resortVacations}
+            setCheckboxState={setResortVacations}
             label="Resort Vacations"
             imageSrc="/assets/icons/coconut-tree.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={wildlife}
+            setCheckboxState={setWildlife}
             label="Wildlife"
             imageSrc="/assets/icons/lion.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={natureAndOutdoors}
+            setCheckboxState={setNatureAndOutdoors}
             label="Luxury Get-aways"
             imageSrc="/assets/icons/diamond.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={luxuryGetAway}
+            setCheckboxState={setLuxuryGetAway}
             label="Active Get-aways"
             imageSrc="/assets/icons/skiing.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={camping}
+            setCheckboxState={setCamping}
             label="Camping"
             imageSrc="/assets/icons/camping-tent.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={burningMan}
+            setCheckboxState={setBurningMan}
             label="Burning Man"
             imageSrc="/assets/icons/fire.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={musicFestivals}
+            setCheckboxState={setMusicFestivals}
             label="Music Festivals"
             imageSrc="/assets/icons/music-notes.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={artAndCulture}
+            setCheckboxState={setArtAndCulture}
             label="Arts & Culture "
             imageSrc="/assets/icons/culture.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={prideEvents}
+            setCheckboxState={setPrideEvents}
             label="Pride events"
             imageSrc="/assets/icons/rainbow.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={wellnessRetreats}
+            setCheckboxState={setWellnessRetreats}
             label="Wellness Retreats"
             imageSrc="/assets/icons/apple.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={volunteeringTrips}
+            setCheckboxState={setVolunteeringTrips}
             label="Volunteering Trips"
             imageSrc="/assets/icons/volunteer.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={cruises}
+            setCheckboxState={setCruises}
             label="Cruises"
             imageSrc="/assets/icons/cruise.png"
           ></ChipCheckBox>
           <ChipCheckBox
+            checkboxState={nudistAdventures}
+            setCheckboxState={setNudistAdventures}
             label="Nudist Adventures"
             imageSrc="/assets/icons/nudist.png"
           ></ChipCheckBox>
