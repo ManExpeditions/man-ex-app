87724f355321f3f061aec7e8c54220ffb53a8485
Ajaydip Singh <singh@minerva.kgi.edu>
2021-11-02 23:02:58 +0300

    :construction: chore: create syling for location options

--- user-client/src/pages/OnboardingPages/LocationPage/LocationPage.js
+++ user-client/src/pages/OnboardingPages/LocationPage/LocationPage.js
@@ -11,34 +11,79 @@ export default function LocationPage(props) {
   };
 
   return (
-    <div className="screen">
-      <Link to="/onboarding/4" className="link link-back">
-        <i class="fas fa-chevron-left fa-fw fa-xs"></i> Back
-      </Link>
-      <h1 className={styles.page_title}>Where are you located?</h1>
-      <main>
-        <p className={`align-center ${styles.info}`}>
-          Enter your location to find events and experiences near you.
-        </p>
-        <div className={styles.location_wrapper}>
-          <i class={`fas fa-map-marker-alt ${styles.location_icon}`}></i>
-          <InputBox
-            isLocation
-            inputState={location}
-            setInputState={setLocation}
-          ></InputBox>
-        </div>
+    <>
+      <div className="screen">
+        <Link to="/onboarding/4" className="link link-back">
+          <i class="fas fa-chevron-left fa-fw fa-xs"></i> Back
+        </Link>
+        <h1 className={styles.page_title}>Where are you located?</h1>
+        <main>
+          <p className={`align-center ${styles.info}`}>
+            Enter your location to find events and experiences near you.
+          </p>
+          <div className={styles.location_wrapper}>
+            <i class={`fas fa-map-marker-alt ${styles.location_icon}`}></i>
+            <InputBox
+              isLocation
+              inputState={location}
+              setInputState={setLocation}
+            ></InputBox>
+          </div>
 
-        <button className={`btn ${styles.location_button}`}>
-          Use My Current Location
-        </button>
-      </main>
+          <button className={`btn ${styles.location_button}`}>
+            Use My Current Location
+          </button>
+        </main>
+        <div className={styles.locations_container}>
+          <h1 className={`screen ${styles.subheading_title}`}>
+            Popular Locations
+          </h1>
+          <ul>
+            <li className={styles.location_item}>
+              <button className={`btn ${styles.location_item_button}`}>
+                <span className={styles.location_city}>San Francisco</span>
+                <br />
+                <span className={styles.location_country}>
+                  California, United States
+                </span>
+              </button>
+            </li>
+            <li className={styles.location_item}>
+              <button className={`btn ${styles.location_item_button}`}>
+                <span className={styles.location_city}>San Francisco</span>
+                <br />
+                <span className={styles.location_country}>
+                  California, United States
+                </span>
+              </button>
+            </li>
+            <li className={styles.location_item}>
+              <button className={`btn ${styles.location_item_button}`}>
+                <span className={styles.location_city}>San Francisco</span>
+                <br />
+                <span className={styles.location_country}>
+                  California, United States
+                </span>
+              </button>
+            </li>
+            <li className={styles.location_item}>
+              <button className={`btn ${styles.location_item_button}`}>
+                <span className={styles.location_city}>San Francisco</span>
+                <br />
+                <span className={styles.location_country}>
+                  California, United States
+                </span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
       <button
         className={`btn btn-primary ${styles.action_button}`}
         onClick={onCompleteHandler}
       >
-        Continue
+        Confirm Location
       </button>
-    </div>
+    </>
   );
 }
