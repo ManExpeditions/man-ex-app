b2d3615b19311d87bd99fe261e43f96aea53eb72
Ajaydip Singh <singh@minerva.kgi.edu>
2021-11-09 20:16:54 +0300

    :art: chore: make verification box dynamic

--- user-client/src/components/DigitVerificationBox/DigitVerificationBox.js
+++ user-client/src/components/DigitVerificationBox/DigitVerificationBox.js
@@ -1,13 +1,6 @@
-import React, { useState } from "react";
 import styles from "./DigitVerificationBox.module.css";
 
-export default function DigitVerificationBox() {
-  const [boxOne, setBoxOne] = useState();
-  const [boxTwo, setBoxTwo] = useState();
-  const [boxThree, setBoxThree] = useState();
-  const [boxFour, setBoxFour] = useState();
-  const [boxFive, setBoxFive] = useState();
-
+export default function DigitVerificationBox({ boxes }) {
   const changeFocusToBox = (boxNumber) => {
     const nextBox = document.getElementById(`box-${boxNumber}`);
     if (nextBox !== null) {
@@ -30,46 +23,17 @@ export default function DigitVerificationBox() {
 
   return (
     <div className={styles.wrapper}>
-      <input
-        autoFocus
-        id="box-1"
-        value={boxOne}
-        onChange={(e) => handleOnChange(1, e.target.value, setBoxOne)}
-        className={`input ${styles.input} ${boxOne && styles.input_filled}`}
-        maxLength={1}
-      ></input>
-      <input
-        id="box-2"
-        value={boxTwo}
-        className={`input ${styles.input} ${boxTwo && styles.input_filled}`}
-        onChange={(e) => handleOnChange(2, e.target.value, setBoxTwo)}
-        onKeyDown={(e) => handleKeyDown(2, e.key, boxTwo)}
-        maxLength={1}
-      ></input>
-      <input
-        id="box-3"
-        value={boxThree}
-        className={`input ${styles.input} ${boxThree && styles.input_filled}`}
-        onChange={(e) => handleOnChange(3, e.target.value, setBoxThree)}
-        onKeyDown={(e) => handleKeyDown(3, e.key, boxThree)}
-        maxLength={1}
-      ></input>
-      <input
-        id="box-4"
-        value={boxFour}
-        className={`input ${styles.input} ${boxFour && styles.input_filled}`}
-        onChange={(e) => handleOnChange(4, e.target.value, setBoxFour)}
-        onKeyDown={(e) => handleKeyDown(4, e.key, boxFour)}
-        maxLength={1}
-      ></input>
-      <input
-        id="box-5"
-        value={boxFive}
-        onChange={(e) => handleOnChange(5, e.target.value, setBoxFive)}
-        onKeyDown={(e) => handleKeyDown(5, e.key, boxFive)}
-        className={`input ${styles.input} ${boxFive && styles.input_filled}`}
-        maxLength={1}
-      ></input>
+      {boxes.map(([boxState, setBoxState], i) => (
+        <input
+          autoFocus={i === 0 ? true : false}
+          id={`box-${i + 1}`}
+          value={boxState}
+          className={`input ${styles.input} ${boxState && styles.input_filled}`}
+          onChange={(e) => handleOnChange(i + 1, e.target.value, setBoxState)}
+          onKeyDown={(e) => handleKeyDown(i + 1, e.key, boxState)}
+          maxLength={1}
+        ></input>
+      ))}
     </div>
   );
 }
