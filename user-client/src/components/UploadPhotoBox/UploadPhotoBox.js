874f9cb01d89904f5087930d45c2826a0f5c4786
Ajaydip Singh <singh@minerva.kgi.edu>
2021-11-10 13:01:13 +0300

    :sparkles: feat: create photo upload box

---
+++ user-client/src/components/UploadPhotoBox/UploadPhotoBox.js
@@ -0,0 +1,30 @@
+import styles from "./UploadPhotoBox.module.css";
+
+export default function UploadPhotoBox({ photoState, setPhotoState }) {
+  const previewPhoto = () => {
+    if (photoState) {
+      return URL.createObjectURL(photoState);
+    }
+  };
+
+  return (
+    <div>
+      <div className={`${styles.container} ${photoState && styles.no_border}`}>
+        <i class="fas fa-plus"></i>
+        <input
+          className={styles.input}
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={(e) => setPhotoState(e.target.files[0])}
+        ></input>
+        {photoState && (
+          <img
+            className={styles.image}
+            src={photoState && previewPhoto()}
+            alt="Profile"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
