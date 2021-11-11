03783f4220a98e6f14773007471b0adf963252c2
Ajaydip Singh <singh@minerva.kgi.edu>
2021-11-11 21:59:43 +0300

    :sparkles: feat: create component that detects clicks outside the component

---
+++ user-client/src/components/OutsideAlerter.js
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from "react";
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+function useOutsideAlerter(ref, setState, stateValue) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setState(stateValue);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, setState, stateValue]);
+}
+
+/**
+ * Component that alerts if you click outside of it
+ */
+export default function OutsideAlerter({ children, setState, stateValue }) {
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setState, stateValue);
+
+  return <div ref={wrapperRef}>{children}</div>;
+}
