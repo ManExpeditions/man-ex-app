f50e1e3e31c8cb933bebc4e7ec8dafcf26b5e016
Ajaydip Singh <singh@minerva.kgi.edu>
2021-11-28 21:56:26 +0300

    :white_check_mark: test: add tests for user update controller

---
+++ backend/src/tests/userTests/userUpdateSpec.ts
@@ -0,0 +1,58 @@
+import supertest from 'supertest';
+import mongoose from 'mongoose';
+import app from '../../app';
+import connect from '../../lib/mongoose';
+import userDao from '../../dao/users/userDao';
+
+// Wrap express app for testing
+const request = supertest(app);
+
+describe('Test user update endpoint', () => {
+  const endpoint = '/api/user/v1/';
+
+  const user_id = mongoose.Types.ObjectId();
+  const user_email = 'john@example.com';
+  const user_pass = 'CyKHe3kR';
+
+  beforeAll(async () => {
+    // Connect to test database
+    connect('mongodb://localhost:27017/user-update');
+    await userDao.create_new_user_by_email(user_email, user_pass, user_id);
+  });
+
+  it('should throw error if body is empty', async () => {
+    const response = await request.put(endpoint + user_id);
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      message: 'Request body is empty.'
+    });
+  });
+
+  it('should throw error if user id does not exist', async () => {
+    const response = await request
+      .put(endpoint + '619fa6092269ffea182c1b6a')
+      .send({ firstName: 'John' });
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      message: 'User does not exist.'
+    });
+  });
+
+  it('should update user firstname', async () => {
+    const response = await request
+      .put(endpoint + user_id)
+      .send({ firstName: 'John' });
+    expect(response.status).toBe(200);
+
+    const updatedUser = await userDao.find_user_by_id(user_id);
+    expect(updatedUser).toBeTruthy();
+    expect(updatedUser?.firstName).toBe(response.body.firstName);
+  });
+
+  afterAll(async () => {
+    // Clean up database after each test
+    await userDao.delete_all_users();
+    // Close database connection after all tests
+    await mongoose.connection.close();
+  });
+});
