e318fbddd53ffafeb7d77c264152ed91f36c4e7a
Ajaydip Singh <singh@minerva.kgi.edu>
2021-12-07 19:46:44 +0300

    :bug: fix: use correct status code for existing user error

--- backend/src/controllers/auth/register/emailRegistrationController.ts
+++ backend/src/controllers/auth/register/emailRegistrationController.ts
@@ -48,9 +48,8 @@ export const emailRegistrationController = [
     const existingUser = await UserDao.find_user_by_email(req.body.email);
     if (existingUser) {
       const err = new Error('User already exists. Please login.');
-      res.status(404);
       logger.error(err.message);
-      res.status(404).json({ message: err.message });
+      res.status(409).json({ message: err.message });
       return;
     }
 
