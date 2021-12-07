cad198ac464408c1378299abfa090ab816e23c75
Ajaydip Singh <singh@minerva.kgi.edu>
2021-12-07 18:23:41 +0300

    :sparkles: feat: create signin slice

--- user-frontend/src/app/store.js
+++ user-frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import emailRegisterSlice from "../slices/auth/emailRegisterSlice";
+import signinSlice from "../slices/auth/signinSlice";
 
 export const store = configureStore({
   reducer: {
     emailRegisterSlice: emailRegisterSlice,
+    signinSlice: signinSlice,
   },
 });
