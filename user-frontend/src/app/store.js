73897331047912157bae96e992bb1f6dad96d836
Ajaydip Singh <singh@minerva.kgi.edu>
2021-12-07 18:23:55 +0300

    :art: chore: save initial state of user signin as user

--- user-frontend/src/app/store.js
+++ user-frontend/src/app/store.js
@@ -3,6 +3,13 @@ import emailRegisterSlice from "../slices/auth/emailRegisterSlice";
 import signinSlice from "../slices/auth/signinSlice";
 
 export const store = configureStore({
+  preloadedState: {
+    signinSlice: {
+      user: localStorage.getItem("user")
+        ? JSON.parse(localStorage.getItem("user"))
+        : null,
+    },
+  },
   reducer: {
     emailRegisterSlice: emailRegisterSlice,
     signinSlice: signinSlice,
