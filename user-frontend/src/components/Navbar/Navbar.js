cc6b281db6bd693730c5c08a798a27ba5b314c62
Ajaydip Singh <singh@minerva.kgi.edu>
2021-12-24 15:15:47 +0300

    :bug: fix: hamburger menu to be relative to top

--- user-frontend/src/components/Navbar/Navbar.js
+++ user-frontend/src/components/Navbar/Navbar.js
@@ -9,100 +9,102 @@ export default function Navbar() {
   return (
     <>
       <header className={styles.header}>
-        <nav className={styles.nav}>
-          <ul className="flex-box">
-            <li className={styles.hamburger_btn}>
-              <div
-                className={hamburgerOpen && "none-pointer"}
-                onClick={() => setHamburgerOpen(true)}
-              >
-                <i class="fas fa-bars fa-2x"></i>
-              </div>
-            </li>
-            <li>
-              <Link to="/home" className="link">
-                <img
-                  className={styles.logo}
-                  src="/logo192.png"
-                  alt="Man Expeditions Logo"
-                />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <nav className={styles.nav}>
-          <ul className={`flex-box ${styles.nav_list_right}`}>
-            <li className={styles.nav_item}>
-              <Link to="/experiences" className="link link-primary">
-                Experiences
-              </Link>
-            </li>
-            <li className={styles.nav_item}>
-              <Link to="/aboutus" className="link link-primary">
-                About Us
-              </Link>
-            </li>
-            <li className={styles.nav_item}>
-              <Link to="/press" className="link link-primary">
-                Press
-              </Link>
-            </li>
-            <li className={styles.nav_item}>
-              <a
-                href="mailto:hello@manexpeditions.com?subject=Contact us"
-                className="link link-primary"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-          <div className={styles.nav_item}>
-            <Link to="/contact" className="link link-primary">
-              Google Translate
-            </Link>
-          </div>
-        </nav>
-      </header>
-      {hamburgerOpen && (
-        <OutsideAlerter setState={setHamburgerOpen} stateValue={false}>
-          <div className={styles.hamburger_menu_wrapper}>
-            <ul className={styles.hamburger_menu_list}>
-              <li className={styles.nav_item}>
-                <Link
-                  to="/experiences"
-                  className={`link link-primary ${styles.hamburger_link}`}
+        <div className={styles.wrapper}>
+          <nav className={styles.nav}>
+            <ul className="flex-box">
+              <li className={styles.hamburger_btn}>
+                <div
+                  className={hamburgerOpen && "none-pointer"}
+                  onClick={() => setHamburgerOpen(true)}
                 >
+                  <i class="fas fa-bars fa-2x"></i>
+                </div>
+              </li>
+              <li>
+                <Link to="/home" className="link">
+                  <img
+                    className={styles.logo}
+                    src="/logo192.png"
+                    alt="Man Expeditions Logo"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <nav className={styles.nav}>
+            <ul className={`flex-box ${styles.nav_list_right}`}>
+              <li className={styles.nav_item}>
+                <Link to="/experiences" className="link link-primary">
                   Experiences
                 </Link>
               </li>
               <li className={styles.nav_item}>
-                <Link
-                  to="/aboutus"
-                  className={`link link-primary ${styles.hamburger_link}`}
-                >
+                <Link to="/aboutus" className="link link-primary">
                   About Us
                 </Link>
               </li>
               <li className={styles.nav_item}>
-                <Link
-                  to="/press"
-                  className={`link link-primary ${styles.hamburger_link}`}
-                >
+                <Link to="/press" className="link link-primary">
                   Press
                 </Link>
               </li>
               <li className={styles.nav_item}>
-                <Link
-                  to="/contact"
-                  className={`link link-primary ${styles.hamburger_link}`}
+                <a
+                  href="mailto:hello@manexpeditions.com?subject=Contact us"
+                  className="link link-primary"
                 >
                   Contact
-                </Link>
+                </a>
               </li>
             </ul>
-          </div>
-        </OutsideAlerter>
-      )}
+            <div className={styles.nav_item}>
+              <Link to="/contact" className="link link-primary">
+                Google Translate
+              </Link>
+            </div>
+          </nav>
+        </div>
+        {hamburgerOpen && (
+          <OutsideAlerter setState={setHamburgerOpen} stateValue={false}>
+            <div className={styles.hamburger_menu_wrapper}>
+              <ul className={styles.hamburger_menu_list}>
+                <li className={styles.nav_item}>
+                  <Link
+                    to="/experiences"
+                    className={`link link-primary ${styles.hamburger_link}`}
+                  >
+                    Experiences
+                  </Link>
+                </li>
+                <li className={styles.nav_item}>
+                  <Link
+                    to="/aboutus"
+                    className={`link link-primary ${styles.hamburger_link}`}
+                  >
+                    About Us
+                  </Link>
+                </li>
+                <li className={styles.nav_item}>
+                  <Link
+                    to="/press"
+                    className={`link link-primary ${styles.hamburger_link}`}
+                  >
+                    Press
+                  </Link>
+                </li>
+                <li className={styles.nav_item}>
+                  <Link
+                    to="/contact"
+                    className={`link link-primary ${styles.hamburger_link}`}
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </OutsideAlerter>
+        )}
+      </header>
     </>
   );
 }
