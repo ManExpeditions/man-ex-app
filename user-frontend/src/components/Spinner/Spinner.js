a1d84f72e1353ba778039aa43c10024ee21f2a85
Ajaydip Singh <singh@minerva.kgi.edu>
2022-01-09 08:22:48 -0800

    :sparkles: feat: create loading spinner

---
+++ user-frontend/src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import { ImSpinner2 } from "react-icons/im";
+
+export default function Spinner() {
+  return (
+    <div className="spinner">
+      <ImSpinner2 className="spinner"></ImSpinner2>
+    </div>
+  );
+}
