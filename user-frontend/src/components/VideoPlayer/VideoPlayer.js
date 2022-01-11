6d15edc28e8c0dad1801a2bfedf52c9224a0b49c
Ajaydip Singh <singh@minerva.kgi.edu>
2022-01-11 08:21:01 -0800

    :bug: fix: set auto for height of video player

--- user-frontend/src/components/VideoPlayer/VideoPlayer.js
+++ user-frontend/src/components/VideoPlayer/VideoPlayer.js
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player";
 export default function VideoPlayer({ src }) {
   return (
     <div>
-      <ReactPlayer url={src} controls width="100%"></ReactPlayer>
+      <ReactPlayer height="auto" url={src} controls width="100%"></ReactPlayer>
     </div>
   );
 }
