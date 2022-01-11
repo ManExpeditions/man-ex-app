70e3fdd2b73c62b1a86919b03a5e4885f04e528d
Ajaydip Singh <singh@minerva.kgi.edu>
2022-01-11 13:15:24 -0800

    :sparkles: feat: add user bottom nav

--- user-frontend/src/components/BottomNav/BottomNav.js
+++ user-frontend/src/components/BottomNav/BottomNav.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./BottomNav.module.css";
 import { FaSearch, FaUser } from "react-icons/fa";
+import { BiEnvelope, BiHeart } from "react-icons/bi";
 
-export default function BottomNav() {
+export default function BottomNav({ user }) {
   return (
     <div className={styles.nav}>
-      <ul className={`flex-box ${styles.nav_list}`}>
+      <ul className={styles.nav_list}>
         <li className={styles.nav_item}>
           <Link
             to="/experiences"
@@ -15,12 +16,56 @@ export default function BottomNav() {
             Explore
           </Link>
         </li>
-        <li className={styles.nav_item}>
-          <Link to="/signin" className={`link link-primary ${styles.nav_link}`}>
-            <FaUser size={18}></FaUser>
-            Login/Register
-          </Link>
-        </li>
+        {user ? (
+          <>
+            <li className={styles.nav_item}>
+              <Link
+                to="/signin"
+                className={`link link-primary ${styles.nav_link}`}
+              >
+                <BiEnvelope size={18}></BiEnvelope>
+                Messages
+              </Link>
+            </li>
+            <li className={styles.nav_item}>
+              <Link
+                to="/signin"
+                className={`link link-primary ${styles.nav_link}`}
+              >
+                <FaUser size={18}></FaUser>
+                Members
+              </Link>
+            </li>
+            <li className={styles.nav_item}>
+              <Link
+                to="/signin"
+                className={`link link-primary ${styles.nav_link}`}
+              >
+                <BiHeart size={18}></BiHeart>
+                Favorites
+              </Link>
+            </li>
+            <li className={styles.nav_item}>
+              <Link
+                to="/signin"
+                className={`link link-primary ${styles.nav_link}`}
+              >
+                <FaUser size={18}></FaUser>
+                Profile
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className={styles.nav_item}>
+            <Link
+              to="/signin"
+              className={`link link-primary ${styles.nav_link}`}
+            >
+              <FaUser size={18}></FaUser>
+              Login/Register
+            </Link>
+          </li>
+        )}
       </ul>
     </div>
   );
