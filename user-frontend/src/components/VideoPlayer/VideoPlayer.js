4f33853cec2e7161902ebd284be01066f6a7fa62
Ajaydip Singh <singh@minerva.kgi.edu>
2022-01-11 09:52:29 -0800

    :lipstick: style: sytle video player

--- user-frontend/src/components/VideoPlayer/VideoPlayer.js
+++ user-frontend/src/components/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,16 @@
 import ReactPlayer from "react-player";
+import styles from "./VideoPlayer.module.css";
 
 export default function VideoPlayer({ src }) {
   return (
-    <div>
-      <ReactPlayer height="auto" url={src} controls width="100%"></ReactPlayer>
+    <div className={styles.container}>
+      <ReactPlayer
+        height="auto"
+        url={src}
+        controls
+        width="100%"
+        style={{ lineHeight: 0 }}
+      ></ReactPlayer>
     </div>
   );
 }
