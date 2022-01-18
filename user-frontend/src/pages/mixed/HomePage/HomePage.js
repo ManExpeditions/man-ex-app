bad800b26d76a9ce97af705cd2fd2687033f3466
Ajaydip Singh <singh@minerva.kgi.edu>
2022-01-18 15:52:47 -0800

    :art: chore: show different content on home if user

--- user-frontend/src/pages/mixed/HomePage/HomePage.js
+++ user-frontend/src/pages/mixed/HomePage/HomePage.js
@@ -158,7 +158,7 @@ export default function HomePage() {
   }, [user]);
 
   return (
-    <div>
+    <div className="page">
       <Navbar user={user}></Navbar>
       <Toast></Toast>
       {!user && (
@@ -221,178 +221,243 @@ export default function HomePage() {
           )}
         </div>
       </main>
-      <section className="bg-white">
-        <div className={`${styles.how_section} ${styles.container}`}>
-          <h1 className={styles.how_section_title}>
-            Imagine finding the perfect trip and the perfect travel buds to
-            enjoy it with?!
-          </h1>
-          <div className={styles.how_section_container}>
-            <img
-              className={styles.how_section_image}
-              src="https://app.greenrope.com/users/myteam46998/Media243.jpg?202106241029"
-              alt="Men holding man expeditions symbol."
-            />
-            <div>
-              <p>
-                For us, choosing a trip to book is equally as important as
-                choosing the perfect people to enjoy it with. What if we told
-                you that we are the matchmaker for trips and like-minded people?
-                Well that's exactly what we are. After you register we will able
-                to match you with like-minded men with similar travel interests.
-              </p>
-              <button
-                className="btn link link-blue"
-                onClick={() => setSeeMore(() => !seeMore)}
-              >
-                {seeMore ? "Hide" : "See more"}
-              </button>
-              {seeMore && (
-                <>
-                  <h2 className={styles.see_more_title}>How it works</h2>
-                  <ul>
-                    <li className={styles.see_more_list_item}>
-                      <b>Step 1:</b> Register & verify your profile
-                    </li>
-                    <li className={styles.see_more_list_item}>
-                      <b>Step 2:</b> When a member who matches your interests
-                      invites you to join their trip, we will notify you...OR
-                      create your own travel group, select your prefered travel
-                      dates and then invite members of the Man Ex community who
-                      share the same travel interests as you.
-                    </li>
-                  </ul>
-                </>
-              )}
+      {!user && (
+        <section className="bg-white">
+          <div className={`${styles.how_section} ${styles.container}`}>
+            <h1 className={styles.how_section_title}>
+              Imagine finding the perfect trip and the perfect travel buds to
+              enjoy it with?!
+            </h1>
+            <div className={styles.how_section_container}>
+              <img
+                className={styles.how_section_image}
+                src="https://app.greenrope.com/users/myteam46998/Media243.jpg?202106241029"
+                alt="Men holding man expeditions symbol."
+              />
+              <div>
+                <p>
+                  For us, choosing a trip to book is equally as important as
+                  choosing the perfect people to enjoy it with. What if we told
+                  you that we are the matchmaker for trips and like-minded
+                  people? Well that's exactly what we are. After you register we
+                  will able to match you with like-minded men with similar
+                  travel interests.
+                </p>
+                <button
+                  className="btn link link-blue"
+                  onClick={() => setSeeMore(() => !seeMore)}
+                >
+                  {seeMore ? "Hide" : "See more"}
+                </button>
+                {seeMore && (
+                  <>
+                    <h2 className={styles.see_more_title}>How it works</h2>
+                    <ul>
+                      <li className={styles.see_more_list_item}>
+                        <b>Step 1:</b> Register & verify your profile
+                      </li>
+                      <li className={styles.see_more_list_item}>
+                        <b>Step 2:</b> When a member who matches your interests
+                        invites you to join their trip, we will notify you...OR
+                        create your own travel group, select your prefered
+                        travel dates and then invite members of the Man Ex
+                        community who share the same travel interests as you.
+                      </li>
+                    </ul>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
       <section className="bg-black">
         <div className={styles.container}>
-          <h1 className={styles.exp_section_heading}>Popular Experience</h1>
-          <div className={styles.exp_section_videos_container}>
-            <div className={styles.video_container}>
-              <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
-              <Link
-                to="/register"
-                className={`link btn-primary ${styles.btn_video_info}`}
-              >
-                Learn more
-              </Link>
-            </div>
-            <div className={styles.video_container}>
-              <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
-              <Link
-                to="/register"
-                className={`link btn-primary ${styles.btn_video_info}`}
-              >
-                Learn more
-              </Link>
-            </div>
-          </div>
+          {user ? (
+            <>
+              <div className={styles.global_exp}>
+                <h1 className={styles.global_exp_heading}>Explore</h1>
+                <h1 className={styles.global_exp_subheading}>
+                  Global Experiences
+                </h1>
+                <div className={styles.global_exp_section_videos_container}>
+                  <div className={styles.global_video_container}>
+                    <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
+                    <Link
+                      to="/register"
+                      className={`link btn-primary ${styles.btn_video_info}`}
+                    >
+                      Learn more
+                    </Link>
+                  </div>
+                  <div className={styles.global_video_container}>
+                    <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
+                    <Link
+                      to="/register"
+                      className={`link btn-primary ${styles.btn_video_info}`}
+                    >
+                      Learn more
+                    </Link>
+                  </div>
+                  <div className={styles.global_video_container}>
+                    <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
+                    <Link
+                      to="/register"
+                      className={`link btn-primary ${styles.btn_video_info}`}
+                    >
+                      Learn more
+                    </Link>
+                  </div>
+                  <div className={styles.global_video_container}>
+                    <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
+                    <Link
+                      to="/register"
+                      className={`link btn-primary ${styles.btn_video_info}`}
+                    >
+                      Learn more
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <h1 className={styles.exp_section_heading}>
+                Popular Experiences
+              </h1>
+              <div className={styles.exp_section_videos_container}>
+                <div className={styles.video_container}>
+                  <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
+                  <Link
+                    to="/register"
+                    className={`link btn-primary ${styles.btn_video_info}`}
+                  >
+                    Learn more
+                  </Link>
+                </div>
+                <div className={styles.video_container}>
+                  <VideoPlayer src="https://res.cloudinary.com/man-expeditions/video/upload/v1640362481/Man_Ex_Cold_Shower_Challenge_-_720WebShareName_nsry8e.mov"></VideoPlayer>
+                  <Link
+                    to="/register"
+                    className={`link btn-primary ${styles.btn_video_info}`}
+                  >
+                    Learn more
+                  </Link>
+                </div>
+              </div>
+            </>
+          )}
+
           <Link to="/experiences" className="link link-long">
             See more experiences
           </Link>
         </div>
       </section>
-      <section className={styles.survey_section}>
-        <img
-          className={styles.survey_section_image}
-          src="https://app.greenrope.com/users/myteam46998/Media240.jpg?202106240524"
-          alt="Men standing during a sunset."
-        />
-        <div className={styles.survey_section_text}>
-          <h2 className={styles.survey_section_title}>
-            Connect with like-minded Traveller's
-          </h2>
-          <Link to="/register" className="link btn btn-primary text-center">
-            Register for free
-          </Link>
-        </div>
-      </section>
-      <section className={`${styles.impact_section} bg-white`}>
-        <div>
-          <h2 className={styles.impact_section_heading}>7 years</h2>
-          <h3 className={styles.impact_section_subheading}>
-            Of leaving our world a better place through travel
-          </h3>
-          <div className={styles.impact_section_blobs_container}>
-            <div className={styles.impact_section_blob}>
-              <img
-                className={styles.impact_section_blob_img}
-                src="https://app.greenrope.com/users/myteam46998/Media380.png?202107300909"
-                alt="Turtle."
-              />
-              <h4 className={styles.impact_section_blob_title}>
-                46,000 Turtles
-              </h4>
-              <p className={styles.impact_section_blob_text}>
-                AVERAGE HATCHED ON BEACHS THAT THE PROCEEDS FROM OUR TRIPS HELP
-                KEEP CLEAN AND SAFE
-              </p>
+      {!user && (
+        <>
+          <section className={styles.survey_section}>
+            <img
+              className={styles.survey_section_image}
+              src="https://app.greenrope.com/users/myteam46998/Media240.jpg?202106240524"
+              alt="Men standing during a sunset."
+            />
+            <div className={styles.survey_section_text}>
+              <h2 className={styles.survey_section_title}>
+                Connect with like-minded Traveller's
+              </h2>
+              <Link to="/register" className="link btn btn-primary text-center">
+                Register for free
+              </Link>
             </div>
-            <div className={styles.impact_section_blob}>
-              <img
-                className={styles.impact_section_blob_img}
-                src="https://app.greenrope.com/users/myteam46998/Media378.png?202107300909"
-                alt="Cycle of plant, animals, and water."
-              />
-              <h4 className={styles.impact_section_blob_title}>
-                1.1 Million Hectares
-              </h4>
-              <p className={styles.impact_section_blob_text}>
-                OF CONSERVATION LAND SUPPORTED FROM THE PROCEEDS OF OUR TRIPS
-              </p>
-            </div>
-            <div className={styles.impact_section_blob}>
-              <img
-                className={styles.impact_section_blob_img}
-                src="https://app.greenrope.com/users/myteam46998/Media377.png?202107300909"
-                alt="Rhino."
-              />
-              <h4 className={styles.impact_section_blob_title}>23 Rhinos</h4>
-              <p className={styles.impact_section_blob_text}>
-                HAVE BEEN RESCUED FROM THE PROCEEDS OF OUR TRIPS
-              </p>
-            </div>
-            <div className={styles.impact_section_blob}>
-              <img
-                className={styles.impact_section_blob_img}
-                src="https://app.greenrope.com/users/myteam46998/Media379.png?202107300909"
-                alt="Whale."
-              />
-              <h4 className={styles.impact_section_blob_title}>
-                2 Thousand Miles
-              </h4>
-              <p className={styles.impact_section_blob_text}>
-                OF COASTLINE SUPPORTED BY OUR TRIPS
-              </p>
-            </div>
-          </div>
+          </section>
+          <section className={`${styles.impact_section} bg-white`}>
+            <div>
+              <h2 className={styles.impact_section_heading}>7 years</h2>
+              <h3 className={styles.impact_section_subheading}>
+                Of leaving our world a better place through travel
+              </h3>
+              <div className={styles.impact_section_blobs_container}>
+                <div className={styles.impact_section_blob}>
+                  <img
+                    className={styles.impact_section_blob_img}
+                    src="https://app.greenrope.com/users/myteam46998/Media380.png?202107300909"
+                    alt="Turtle."
+                  />
+                  <h4 className={styles.impact_section_blob_title}>
+                    46,000 Turtles
+                  </h4>
+                  <p className={styles.impact_section_blob_text}>
+                    AVERAGE HATCHED ON BEACHS THAT THE PROCEEDS FROM OUR TRIPS
+                    HELP KEEP CLEAN AND SAFE
+                  </p>
+                </div>
+                <div className={styles.impact_section_blob}>
+                  <img
+                    className={styles.impact_section_blob_img}
+                    src="https://app.greenrope.com/users/myteam46998/Media378.png?202107300909"
+                    alt="Cycle of plant, animals, and water."
+                  />
+                  <h4 className={styles.impact_section_blob_title}>
+                    1.1 Million Hectares
+                  </h4>
+                  <p className={styles.impact_section_blob_text}>
+                    OF CONSERVATION LAND SUPPORTED FROM THE PROCEEDS OF OUR
+                    TRIPS
+                  </p>
+                </div>
+                <div className={styles.impact_section_blob}>
+                  <img
+                    className={styles.impact_section_blob_img}
+                    src="https://app.greenrope.com/users/myteam46998/Media377.png?202107300909"
+                    alt="Rhino."
+                  />
+                  <h4 className={styles.impact_section_blob_title}>
+                    23 Rhinos
+                  </h4>
+                  <p className={styles.impact_section_blob_text}>
+                    HAVE BEEN RESCUED FROM THE PROCEEDS OF OUR TRIPS
+                  </p>
+                </div>
+                <div className={styles.impact_section_blob}>
+                  <img
+                    className={styles.impact_section_blob_img}
+                    src="https://app.greenrope.com/users/myteam46998/Media379.png?202107300909"
+                    alt="Whale."
+                  />
+                  <h4 className={styles.impact_section_blob_title}>
+                    2 Thousand Miles
+                  </h4>
+                  <p className={styles.impact_section_blob_text}>
+                    OF COASTLINE SUPPORTED BY OUR TRIPS
+                  </p>
+                </div>
+              </div>
 
-          <div className={styles.container}>
-            <h2 className={styles.impact_section_heading}>
-              Our world doesn't have time for average.
-            </h2>
-            <br />
-            <h4>
-              That's why we need traveller's like you, who seek to go beyond
-              average travel - to travel experiences that leave our world a
-              better place.
-            </h4>
-            <br />
-            <p>
-              For 7 years, Man Expeditions has been building a vision that,
-              through the support of our travel community, allows us to run
-              group trips and experiences that all help fund important wildlife
-              and environmental conservation efforts. We'd like to take you to
-              three continents to discover extraordinary landscapes in a travel
-              dream that goes beyond the expected
-            </p>
-          </div>
-        </div>
-      </section>
+              <div className={styles.container}>
+                <h2 className={styles.impact_section_heading}>
+                  Our world doesn't have time for average.
+                </h2>
+                <br />
+                <h4>
+                  That's why we need traveller's like you, who seek to go beyond
+                  average travel - to travel experiences that leave our world a
+                  better place.
+                </h4>
+                <br />
+                <p>
+                  For 7 years, Man Expeditions has been building a vision that,
+                  through the support of our travel community, allows us to run
+                  group trips and experiences that all help fund important
+                  wildlife and environmental conservation efforts. We'd like to
+                  take you to three continents to discover extraordinary
+                  landscapes in a travel dream that goes beyond the expected
+                </p>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+
       <BottomNav user={user}></BottomNav>
     </div>
   );
