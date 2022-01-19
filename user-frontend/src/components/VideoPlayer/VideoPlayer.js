2da434c7008d67a627f7b5d2a4295312314c8e38
Ajaydip Singh <singh@minerva.kgi.edu>
2022-01-18 16:22:13 -0800

    :sparkles: feat: allow ability to set thumbnail on video player

--- user-frontend/src/components/VideoPlayer/VideoPlayer.js
+++ user-frontend/src/components/VideoPlayer/VideoPlayer.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import ReactPlayer from "react-player";
 import styles from "./VideoPlayer.module.css";
 
-export default function VideoPlayer({ src }) {
+export default function VideoPlayer({ src, thumbnail, videoTitle }) {
+  const [thumbnailVisible, setThumbnailVisible] = useState(true);
+
   return (
     <div className={styles.container}>
+      {thumbnailVisible && thumbnail && (
+        <img
+          className={styles.thumbnail}
+          src={thumbnail}
+          alt={videoTitle ? videoTitle : "Video thumbnail."}
+        />
+      )}
       <ReactPlayer
         height="auto"
         url={src}
         controls
         width="100%"
         style={{ lineHeight: 0 }}
+        onPlay={() => setThumbnailVisible(false)}
+        onEnded={() => setThumbnailVisible(true)}
       ></ReactPlayer>
     </div>
   );
