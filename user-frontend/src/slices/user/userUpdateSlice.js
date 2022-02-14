e2c1cefedfab6f9f1e066f0206bf5318b69ad79c
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-14 10:34:12 -0800

    :bug: fix: add token property explicitly for user in local storage

--- user-frontend/src/slices/user/userUpdateSlice.js
+++ user-frontend/src/slices/user/userUpdateSlice.js
@@ -15,7 +15,8 @@ export const userUpdate = createAsyncThunk(
     } = getState();
     try {
       const data = await userAPI.updateUser(user.id, user.token, userData);
-      localStorage.setItem("user", JSON.stringify({ ...data, ...user }));
+      const token = user.token;
+      localStorage.setItem("user", JSON.stringify({ ...data, token }));
       return data;
     } catch (err) {
       if (!err.response) {
