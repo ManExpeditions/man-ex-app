cfae6e886734850a7f9f4aa3d1b3fbb5063cdd6f
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-14 10:32:27 -0800

    :art: chore: create a set interests function

--- user-frontend/src/utils/common.js
+++ user-frontend/src/utils/common.js
@@ -24,3 +24,71 @@ export const getOnboardingRedirectPage = (user) => {
   }
   return redirect;
 };
+
+export const setInterestStates = (
+  userInterests,
+  {
+    setArtAndCulture,
+    setBurningMan,
+    setCamping,
+    setCruises,
+    setLuxuryGetAway,
+    setMusicFestivals,
+    setNatureAndOutdoors,
+    setNudistAdventures,
+    setPrideEvents,
+    setResortVacations,
+    setVolunteeringTrips,
+    setWellnessRetreats,
+    setWildlife,
+    setActiveGetAway,
+  }
+) => {
+  // If there are no interests, return early
+  if (!userInterests) {
+    return;
+  }
+
+  if (userInterests.includes("Arts & Culture")) {
+    setArtAndCulture(true);
+  }
+  if (userInterests.includes("Burning Man")) {
+    setBurningMan(true);
+  }
+  if (userInterests.includes("Camping")) {
+    setCamping(true);
+  }
+  if (userInterests.includes("Cruises")) {
+    setCruises(true);
+  }
+  if (userInterests.includes("Luxury Get-aways")) {
+    setLuxuryGetAway(true);
+  }
+  if (userInterests.includes("Music Festivals")) {
+    setMusicFestivals(true);
+  }
+  if (userInterests.includes("Nature & Outdoors")) {
+    setNatureAndOutdoors(true);
+  }
+  if (userInterests.includes("Nudist Adventures")) {
+    setNudistAdventures(true);
+  }
+  if (userInterests.includes("Pride Events")) {
+    setPrideEvents(true);
+  }
+  if (userInterests.includes("Resort Vacations")) {
+    setResortVacations(true);
+  }
+  if (userInterests.includes("Volunteering Trips")) {
+    setVolunteeringTrips(true);
+  }
+  if (userInterests.includes("Wellness Retreats")) {
+    setWellnessRetreats(true);
+  }
+  if (userInterests.includes("Wildlife")) {
+    setWildlife(true);
+  }
+  if (userInterests.includes("Active Get-aways")) {
+    setActiveGetAway(true);
+  }
+};
