202de72ad2f220a5ed499d66de6bad92e9528e87
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-15 19:22:02 -0800

    :art: chore: forward ref from higher order components

--- user-frontend/src/higherOrderComponents/withPasswordToggle.js
+++ user-frontend/src/higherOrderComponents/withPasswordToggle.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 
 const withPasswordToggle = (component) => {
   const Component = component;
-  return ({ type, ...props }) => {
+  return forwardRef(({ type, ...props }, ref) => {
     const [inputType, setInputType] = useState(type);
 
     const togglePasswordVisibility = () => {
@@ -15,7 +15,7 @@ const withPasswordToggle = (component) => {
 
     return (
       <div className="pos-relative">
-        <Component type={inputType} {...props} />
+        <Component type={inputType} {...props} ref={ref} />
         {type === "password" && (
           <button className="eye-button" onClick={togglePasswordVisibility}>
             <i class="fa fa-eye eye" aria-hidden="true"></i>
@@ -23,7 +23,7 @@ const withPasswordToggle = (component) => {
         )}
       </div>
     );
-  };
+  });
 };
 
 export default withPasswordToggle;
