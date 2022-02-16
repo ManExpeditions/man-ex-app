98e5cb0ba2be76511152ecab8ce12c1ee03dd671
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-15 18:23:39 -0800

    :sparkles: feat: create endpoint for validating user password

---
+++ backend/src/controllers/user/userValidateController.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import { body, param, validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
+
+import logger from '../../lib/logger';
+import userDao from '../../dao/users/userDao';
+import { isAuthenticated } from '../../middleware/authMiddleware';
+
+/**
+ * @api {post} /user/v1/:id/validate Validate user by checking password
+ * @apiDescription Validate an existing user by email.
+ * @apiPermission Authentication
+ * @apiVersion 1.0.0
+ * @apiName ValidateUser
+ * @apiGroup User
+ *
+ * @apiParam {String} id Users unique ID.
+ *
+ * @apiBody {String} password The password of the user.
+ *
+ * @apiSuccess {String} Success message.
+ *
+ */
+export const userValidateController = [
+  // Has to an authenticated request
+  isAuthenticated,
+  // Sanitize and validate body params
+  body('password', 'Please enter a password').isLength({ min: 1 }).escape(),
+  // Validate param
+  param('id', 'Id param must be a string')
+    .isLength({ min: 5 })
+    .isString()
+    .escape(),
+
+  expressAsyncHandler(async function (req: Request, res: Response) {
+    // Find the validation errors from the request.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      logger.error(errors);
+      res.status(404).json(errors);
+      return;
+    }
+
+    // Check if user does not exist
+    const user = await userDao.find_user_by_id(req.params.id);
+    if (!user) {
+      const err = new Error('User does not exist. Please register.');
+      logger.error(err.message);
+      res.status(404).json({ message: err.message });
+      return;
+    }
+
+    // Validate if passwords match
+    const passwordsMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordsMatch) {
+      const err = new Error('Incorrect password. Validation failed.');
+      logger.error(err.message);
+      res.status(401).send({ message: err.message });
+      return;
+    }
+
+    res.status(200).json({ message: 'User validated.' });
+    return;
+  })
+];
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import { userUpdateController } from '../controllers/user/userUpdateController';
 import { userVerifyEmailController } from '../controllers/user/userVerifyEmailController';
 import { userVerifyPhoneController } from '../controllers/user/userVerifyPhoneController';
 import { userVerificationCodeController } from '../controllers/user/userVerificationCodeController';
+import { userValidateController } from '../controllers/user/userValidateController';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.put('/:id', userUpdateController);
 router.post('/:id/verify/email', userVerifyEmailController);
 router.post('/:id/verify/phone', userVerifyPhoneController);
 router.post('/:id/verify/code', userVerificationCodeController);
+router.post('/:id/validate', userValidateController);
 
 export default router;
