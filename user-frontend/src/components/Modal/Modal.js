deb7fba5ecc9c958023fb61e4516bbad664fd14e
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-18 17:02:36 -0800

    :sparkles: feat: create modal component

---
+++ user-frontend/src/components/Modal/Modal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import OutsideAlerter from "../OutsideAlerter";
+import styles from "./Modal.module.css";
+
+export default function Modal({ children, setIsOpen, ...props }) {
+  return (
+    <div className={styles.modal} {...props}>
+      <OutsideAlerter setState={setIsOpen} stateValue={false}>
+        <div className={styles.children}>{children}</div>
+      </OutsideAlerter>
+    </div>
+  );
+}
