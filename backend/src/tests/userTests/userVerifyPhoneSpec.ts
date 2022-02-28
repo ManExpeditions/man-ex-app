ce7d14a188bec0ddbb3e4848a79451c5996e69ca
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-28 15:16:18 -0800

    :white_check_mark: test: add tests for verify phone endpoint

---
+++ backend/src/tests/userTests/userVerifyPhoneSpec.ts
@@ -0,0 +1,62 @@
+import supertest from 'supertest';
+import mongoose from 'mongoose';
+import config from '../../env';
+import app from '../../app';
+import connect from '../../lib/mongoose';
+import userDao from '../../dao/users/userDao';
+import generateToken from '../../lib/jwt';
+
+// Wrap express app for testing
+const request = supertest(app);
+
+describe('Test user verify phone endpoint', () => {
+  const user_id = mongoose.Types.ObjectId();
+  const user_email = 'john@example.com';
+  const user_pass = 'CyKHe3kR';
+  const user_token = generateToken({ _id: user_id });
+
+  const endpoint = config.test.user.base_endpoint + `${user_id}/verify/phone`;
+
+  beforeAll(async () => {
+    // Connect to test database
+    const dbName = 'user-verify-email';
+    connect(config.test.base_db_path + dbName);
+
+    await userDao.create_new_user_by_email(user_email, user_pass, user_id);
+  });
+
+  it('should throw error if no authorization header', async () => {
+    const response = await request
+      .post(endpoint)
+      .send({ verification_code: '123432', email: user_email });
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({
+      message: 'Invalid Request: Request missing Authorization header'
+    });
+  });
+
+  it('should throw error if no verification code', async () => {
+    const response = await request
+      .post(endpoint)
+      .set('Authorization', `Bearer ${user_token}`)
+      .send({ phone: user_email });
+    expect(response.status).toBe(404);
+  });
+
+  it('should throw error if no phone', async () => {
+    const response = await request
+      .post(endpoint)
+      .set('Authorization', `Bearer ${user_token}`)
+      .send({ verification_code: '123245' });
+    expect(response.status).toBe(404);
+  });
+
+  afterAll(async () => {
+    // Clean up database after each test
+    await userDao.delete_all_users();
+    // Delete the current database
+    await mongoose.connection.db.dropDatabase();
+    // Close database connection after all tests
+    await mongoose.connection.close();
+  });
+});
