2ba0eef7bfeb3dff34bd70e2a51807a8a78b9586
Ajaydip Singh <singh@minerva.kgi.edu>
2022-02-27 16:39:20 -0800

    :white_check_mark: chore: add more tests for verify email

--- backend/src/tests/userTests/userVerifyEmailSpec.ts
+++ backend/src/tests/userTests/userVerifyEmailSpec.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import mongoose from 'mongoose';
+import config from '../../env';
 import app from '../../app';
 import connect from '../../lib/mongoose';
 import userDao from '../../dao/users/userDao';
@@ -12,12 +13,15 @@ describe('Test user verify email endpoint', () => {
   const user_id = mongoose.Types.ObjectId();
   const user_email = 'john@example.com';
   const user_pass = 'CyKHe3kR';
+  const user_token = generateToken({ _id: user_id });
 
-  const endpoint = `/api/user/v1/${user_id}/verify/email`;
+  const endpoint = config.test.user.base_endpoint + `${user_id}/verify/email`;
 
   beforeAll(async () => {
     // Connect to test database
-    connect('mongodb://localhost:27017/user-verify-email');
+    const dbName = 'user-verify-email';
+    connect(config.test.base_db_path + dbName);
+
     await userDao.create_new_user_by_email(user_email, user_pass, user_id);
   });
 
@@ -31,9 +35,27 @@ describe('Test user verify email endpoint', () => {
     });
   });
 
+  it('should throw error if no verification code', async () => {
+    const response = await request
+      .post(endpoint)
+      .set('Authorization', `Bearer ${user_token}`)
+      .send({ email: user_email });
+    expect(response.status).toBe(404);
+  });
+
+  it('should throw error if no email', async () => {
+    const response = await request
+      .post(endpoint)
+      .set('Authorization', `Bearer ${user_token}`)
+      .send({ verification_code: '123245' });
+    expect(response.status).toBe(404);
+  });
+
   afterAll(async () => {
     // Clean up database after each test
     await userDao.delete_all_users();
+    // Delete the current database
+    await mongoose.connection.db.dropDatabase();
     // Close database connection after all tests
     await mongoose.connection.close();
   });
