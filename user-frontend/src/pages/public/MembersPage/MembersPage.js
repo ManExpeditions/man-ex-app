a120530aff0ed35c51cd2dd14d76d7084899dfc2
Ajaydip Singh <singh@minerva.kgi.edu>
2022-08-19 21:28:36 -0700

    :lipstick: style: add intro text to members page

--- user-frontend/src/pages/public/MembersPage/MembersPage.js
+++ user-frontend/src/pages/public/MembersPage/MembersPage.js
@@ -35,9 +35,19 @@ export default function MembersPage() {
     <div>
       <Navbar user={user}></Navbar>
       <div className={styles.wrapper}>
-        <div className={styles.profiles_container}>
-          {users &&
-            users.map((user) => <ProfileBox key={user._id} user={user} />)}
+        <div className={styles.container}>
+          <div>
+            <h1 className={styles.hero_text_heading}>Discover</h1>
+            <h1 className={styles.hero_text_subheading}>like-minded Travellers</h1>
+            <p>
+              Get matched with like-minded travelers with upcoming trips/events
+              and create extraordinary memories together
+            </p>
+          </div>
+          <div className={styles.profiles_container}>
+            {users &&
+              users.map((user) => <ProfileBox key={user._id} user={user} />)}
+          </div>
         </div>
       </div>
       <BottomNav members user={user} />
